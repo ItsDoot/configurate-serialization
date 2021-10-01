bd4fd28118bde2535b4191217b06fcfa20d6077f
Jesse McKee <jessemckee97@gmail.com>
2021-10-01 00:30:02 -0500

    Make jitpack work Apparently you need to give gradlew permission to run, depsite this not having a visual difference, it apparently should work

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     api("org.spongepowered:configurate-core:4.1.2")
 
     testImplementation("org.spongepowered:configurate-hocon:4.1.2")
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13.2")
 }
 
 configure<JavaPluginConvention> {
